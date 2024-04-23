bf3de47069cba666caed3fbcab55c82048350f67
Hüseyin <hyegen205@gmail.com>
2024-04-23 21:07:48 +0300

    WebService added.

---
+++ YGN.Store.Management.Web.Service/Models/WebServiceManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace YGN.Store.Management.Web.Service.Models
+{
+    public class WebServiceManager
+    {
+        public int Result { get; set; }
+        public object Data { get; set; }
+    }
+}
