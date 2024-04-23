1ee4a32663bdc3ea7155c314ddcd755c6713eb3c
Hüseyin <hyegen205@gmail.com>
2024-04-24 00:20:25 +0300

    rdlc updated

--- YGN.Store.Management.UI/Report/PrintOrderSlip.Designer.cs
+++ YGN.Store.Management.UI/Report/PrintOrderSlip.Designer.cs
@@ -65,7 +65,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(877, 778);
+            this.ClientSize = new System.Drawing.Size(764, 748);
             this.Controls.Add(this.btnPrintOrderDetail);
             this.Controls.Add(this.txtOrderId);
             this.Controls.Add(this.reportViewer1);
