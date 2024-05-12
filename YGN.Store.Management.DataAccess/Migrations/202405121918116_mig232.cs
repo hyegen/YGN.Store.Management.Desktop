61ad8cdeb81a4646410b56700627b4bd7b459a62
Hüseyin <hyegen205@gmail.com>
2024-05-12 22:43:37 +0300

    migS.

---
+++ YGN.Store.Management.DataAccess/Migrations/202405121918116_mig232.cs
@@ -0,0 +1,16 @@
+﻿namespace YGN.Store.Management.DataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class mig232 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
